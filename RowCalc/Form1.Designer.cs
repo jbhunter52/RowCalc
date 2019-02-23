67997a6e312702638f71d5bd45bf08526dffb087
Jared <jbhunter52@yahoo.com>
2019-02-23 13:16:29 -0600

    updates

--- RowCalc/Form1.Designer.cs
+++ RowCalc/Form1.Designer.cs
@@ -31,16 +31,13 @@
             this.TextBox6 = new System.Windows.Forms.TextBox();
             this.TextBox5 = new System.Windows.Forms.TextBox();
             this.TextBox4 = new System.Windows.Forms.TextBox();
-            this.Button5 = new System.Windows.Forms.Button();
-            this.Button4 = new System.Windows.Forms.Button();
-            this.Button3 = new System.Windows.Forms.Button();
-            this.Button2 = new System.Windows.Forms.Button();
             this.Label3 = new System.Windows.Forms.Label();
-            this.TextBox3 = new System.Windows.Forms.TextBox();
-            this.TextBox2 = new System.Windows.Forms.TextBox();
-            this.Button1 = new System.Windows.Forms.Button();
-            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.TextBox500msec = new System.Windows.Forms.TextBox();
+            this.TextBox500mmin = new System.Windows.Forms.TextBox();
+            this.TextBoxCalhr = new System.Windows.Forms.TextBox();
             this.Label2 = new System.Windows.Forms.Label();
+            this.TextBoxCalMin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TextBox6
@@ -64,80 +61,35 @@
             this.TextBox4.Size = new System.Drawing.Size(78, 20);
             this.TextBox4.TabIndex = 24;
             // 
-            // Button5
-            // 
-            this.Button5.Location = new System.Drawing.Point(190, 159);
-            this.Button5.Name = "Button5";
-            this.Button5.Size = new System.Drawing.Size(51, 23);
-            this.Button5.TabIndex = 23;
-            this.Button5.Text = "Button5";
-            this.Button5.UseVisualStyleBackColor = true;
-            // 
-            // Button4
-            // 
-            this.Button4.Location = new System.Drawing.Point(190, 130);
-            this.Button4.Name = "Button4";
-            this.Button4.Size = new System.Drawing.Size(51, 23);
-            this.Button4.TabIndex = 22;
-            this.Button4.Text = "Button4";
-            this.Button4.UseVisualStyleBackColor = true;
-            // 
-            // Button3
-            // 
-            this.Button3.Location = new System.Drawing.Point(190, 101);
-            this.Button3.Name = "Button3";
-            this.Button3.Size = new System.Drawing.Size(51, 23);
-            this.Button3.TabIndex = 21;
-            this.Button3.Text = "Button3";
-            this.Button3.UseVisualStyleBackColor = true;
-            // 
-            // Button2
-            // 
-            this.Button2.Location = new System.Drawing.Point(190, 35);
-            this.Button2.Name = "Button2";
-            this.Button2.Size = new System.Drawing.Size(51, 23);
-            this.Button2.TabIndex = 20;
-            this.Button2.Text = "Button2";
-            this.Button2.UseVisualStyleBackColor = true;
-            // 
             // Label3
             // 
             this.Label3.AutoSize = true;
-            this.Label3.Location = new System.Drawing.Point(28, 41);
+            this.Label3.Location = new System.Drawing.Point(28, 67);
             this.Label3.Name = "Label3";
             this.Label3.Size = new System.Drawing.Size(72, 13);
             this.Label3.TabIndex = 19;
             this.Label3.Text = "500m min:sec";
             // 
-            // TextBox3
+            // TextBox500msec
             // 
-            this.TextBox3.Location = new System.Drawing.Point(148, 38);
-            this.TextBox3.Name = "TextBox3";
-            this.TextBox3.Size = new System.Drawing.Size(36, 20);
-            this.TextBox3.TabIndex = 18;
+            this.TextBox500msec.Location = new System.Drawing.Point(142, 64);
+            this.TextBox500msec.Name = "TextBox500msec";
+            this.TextBox500msec.Size = new System.Drawing.Size(42, 20);
+            this.TextBox500msec.TabIndex = 18;
             // 
-            // TextBox2
+            // TextBox500mmin
             // 
-            this.TextBox2.Location = new System.Drawing.Point(106, 38);
-            this.TextBox2.Name = "TextBox2";
-            this.TextBox2.Size = new System.Drawing.Size(36, 20);
-            this.TextBox2.TabIndex = 17;
+            this.TextBox500mmin.Location = new System.Drawing.Point(106, 64);
+            this.TextBox500mmin.Name = "TextBox500mmin";
+            this.TextBox500mmin.Size = new System.Drawing.Size(30, 20);
+            this.TextBox500mmin.TabIndex = 17;
             // 
-            // Button1
+            // TextBoxCalhr
             // 
-            this.Button1.Location = new System.Drawing.Point(190, 9);
-            this.Button1.Name = "Button1";
-            this.Button1.Size = new System.Drawing.Size(51, 23);
-            this.Button1.TabIndex = 16;
-            this.Button1.Text = "Button1";
-            this.Button1.UseVisualStyleBackColor = true;
-            // 
-            // TextBox1
-            // 
-            this.TextBox1.Location = new System.Drawing.Point(106, 12);
-            this.TextBox1.Name = "TextBox1";
-            this.TextBox1.Size = new System.Drawing.Size(78, 20);
-            this.TextBox1.TabIndex = 15;
+            this.TextBoxCalhr.Location = new System.Drawing.Point(106, 12);
+            this.TextBoxCalhr.Name = "TextBoxCalhr";
+            this.TextBoxCalhr.Size = new System.Drawing.Size(78, 20);
+            this.TextBoxCalhr.TabIndex = 15;
             // 
             // Label2
             // 
@@ -148,26 +100,41 @@
             this.Label2.TabIndex = 14;
             this.Label2.Text = "Cal/hr";
             // 
+            // TextBoxCalMin
+            // 
+            this.TextBoxCalMin.Location = new System.Drawing.Point(106, 37);
+            this.TextBoxCalMin.Name = "TextBoxCalMin";
+            this.TextBoxCalMin.Size = new System.Drawing.Size(78, 20);
+            this.TextBoxCalMin.TabIndex = 28;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Cal/min";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(297, 267);
+            this.Controls.Add(this.TextBoxCalMin);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.TextBox6);
             this.Controls.Add(this.TextBox5);
             this.Controls.Add(this.TextBox4);
-            this.Controls.Add(this.Button5);
-            this.Controls.Add(this.Button4);
-            this.Controls.Add(this.Button3);
-            this.Controls.Add(this.Button2);
             this.Controls.Add(this.Label3);
-            this.Controls.Add(this.TextBox3);
-            this.Controls.Add(this.TextBox2);
-            this.Controls.Add(this.Button1);
-            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.TextBox500msec);
+            this.Controls.Add(this.TextBox500mmin);
+            this.Controls.Add(this.TextBoxCalhr);
             this.Controls.Add(this.Label2);
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -178,16 +145,13 @@
         internal System.Windows.Forms.TextBox TextBox6;
         internal System.Windows.Forms.TextBox TextBox5;
         internal System.Windows.Forms.TextBox TextBox4;
-        internal System.Windows.Forms.Button Button5;
-        internal System.Windows.Forms.Button Button4;
-        internal System.Windows.Forms.Button Button3;
-        internal System.Windows.Forms.Button Button2;
         internal System.Windows.Forms.Label Label3;
-        internal System.Windows.Forms.TextBox TextBox3;
-        internal System.Windows.Forms.TextBox TextBox2;
-        internal System.Windows.Forms.Button Button1;
-        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.TextBox TextBox500msec;
+        internal System.Windows.Forms.TextBox TextBox500mmin;
+        internal System.Windows.Forms.TextBox TextBoxCalhr;
         internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.TextBox TextBoxCalMin;
+        internal System.Windows.Forms.Label label1;
     }
 }
 
--- RowCalc/Form1.cs
+++ RowCalc/Form1.cs
@@ -16,5 +16,61 @@ namespace RowCalc
         {
             InitializeComponent();
         }
+
+        private void ButtonCalc_calhr_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private double GetSecFromCalHr(double calhr)
+        {
+            return -2.4424e-8 * Math.Pow(calhr, 3) + 1.3114e-4* Math.Pow(calhr, 2) - 2.5303e-1 * calhr + 266.14;
+        }
+        public double GetCalHrFromSec(double sec)
+        {
+            return -4.49e-3 * Math.Pow(sec, 3) + 1.96 * Math.Pow(sec, 2) - 2.94e2 * sec + 15800;
+        }
+
+        private void ButtonCalc_500m_Click(object sender, EventArgs e)
+        {
+            double totsec = int.Parse(TextBox500mmin.Text) * 60 + double.Parse(TextBox500msec.Text);
+            double calhr = GetCalHrFromSec(totsec);
+            TextBoxCalhr.Text = calhr.ToString();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (TextBoxCalhr.Focused)
+                {
+                    if (TextBoxCalhr.Text != "")
+                    {
+                        double calhr = double.Parse(TextBoxCalhr.Text);
+                        double totsec = GetSecFromCalHr(calhr);
+                        int min = (int)Math.Floor(totsec / 60);
+                        double sec = totsec - min * 60;
+                        TextBoxCalMin.Text = (calhr / 60).ToString("0.#");
+                        TextBox500mmin.Text = min.ToString();
+                        TextBox500msec.Text = sec.ToString("0.#");
+                    }
+                }
+
+                if (TextBoxCalMin.Focused)
+                {
+                    if (TextBoxCalMin.Text != "")
+                    {
+                        double calmin = double.Parse(TextBoxCalMin.Text);
+                        double totsec = GetSecFromCalHr(calmin*60);
+                        int min = (int)Math.Floor(totsec / 60);
+                        double sec = totsec - min * 60;
+                        TextBoxCalhr.Text = (calmin * 60).ToString("0.#");
+                        TextBox500mmin.Text = min.ToString();
+                        TextBox500msec.Text = sec.ToString("0.#");
+                    }
+                }
+            }
+        }
+
     }
 }
