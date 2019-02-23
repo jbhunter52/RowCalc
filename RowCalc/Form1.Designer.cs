4e83fc5c23989177cb75cbed541b555b04e7e8c8
Jared <jbhunter52@yahoo.com>
2019-02-22 23:30:23 -0600

    first commit

---
+++ RowCalc/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace RowCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox6 = new System.Windows.Forms.TextBox();
+            this.TextBox5 = new System.Windows.Forms.TextBox();
+            this.TextBox4 = new System.Windows.Forms.TextBox();
+            this.Button5 = new System.Windows.Forms.Button();
+            this.Button4 = new System.Windows.Forms.Button();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.TextBox3 = new System.Windows.Forms.TextBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBox6
+            // 
+            this.TextBox6.Location = new System.Drawing.Point(106, 162);
+            this.TextBox6.Name = "TextBox6";
+            this.TextBox6.Size = new System.Drawing.Size(78, 20);
+            this.TextBox6.TabIndex = 26;
+            // 
+            // TextBox5
+            // 
+            this.TextBox5.Location = new System.Drawing.Point(106, 133);
+            this.TextBox5.Name = "TextBox5";
+            this.TextBox5.Size = new System.Drawing.Size(78, 20);
+            this.TextBox5.TabIndex = 25;
+            // 
+            // TextBox4
+            // 
+            this.TextBox4.Location = new System.Drawing.Point(106, 104);
+            this.TextBox4.Name = "TextBox4";
+            this.TextBox4.Size = new System.Drawing.Size(78, 20);
+            this.TextBox4.TabIndex = 24;
+            // 
+            // Button5
+            // 
+            this.Button5.Location = new System.Drawing.Point(190, 159);
+            this.Button5.Name = "Button5";
+            this.Button5.Size = new System.Drawing.Size(51, 23);
+            this.Button5.TabIndex = 23;
+            this.Button5.Text = "Button5";
+            this.Button5.UseVisualStyleBackColor = true;
+            // 
+            // Button4
+            // 
+            this.Button4.Location = new System.Drawing.Point(190, 130);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(51, 23);
+            this.Button4.TabIndex = 22;
+            this.Button4.Text = "Button4";
+            this.Button4.UseVisualStyleBackColor = true;
+            // 
+            // Button3
+            // 
+            this.Button3.Location = new System.Drawing.Point(190, 101);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(51, 23);
+            this.Button3.TabIndex = 21;
+            this.Button3.Text = "Button3";
+            this.Button3.UseVisualStyleBackColor = true;
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(190, 35);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(51, 23);
+            this.Button2.TabIndex = 20;
+            this.Button2.Text = "Button2";
+            this.Button2.UseVisualStyleBackColor = true;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(28, 41);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(72, 13);
+            this.Label3.TabIndex = 19;
+            this.Label3.Text = "500m min:sec";
+            // 
+            // TextBox3
+            // 
+            this.TextBox3.Location = new System.Drawing.Point(148, 38);
+            this.TextBox3.Name = "TextBox3";
+            this.TextBox3.Size = new System.Drawing.Size(36, 20);
+            this.TextBox3.TabIndex = 18;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(106, 38);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(36, 20);
+            this.TextBox2.TabIndex = 17;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(190, 9);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(51, 23);
+            this.Button1.TabIndex = 16;
+            this.Button1.Text = "Button1";
+            this.Button1.UseVisualStyleBackColor = true;
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(106, 12);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(78, 20);
+            this.TextBox1.TabIndex = 15;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(64, 15);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(36, 13);
+            this.Label2.TabIndex = 14;
+            this.Label2.Text = "Cal/hr";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.TextBox6);
+            this.Controls.Add(this.TextBox5);
+            this.Controls.Add(this.TextBox4);
+            this.Controls.Add(this.Button5);
+            this.Controls.Add(this.Button4);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.TextBox3);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.Label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.TextBox TextBox6;
+        internal System.Windows.Forms.TextBox TextBox5;
+        internal System.Windows.Forms.TextBox TextBox4;
+        internal System.Windows.Forms.Button Button5;
+        internal System.Windows.Forms.Button Button4;
+        internal System.Windows.Forms.Button Button3;
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox TextBox3;
+        internal System.Windows.Forms.TextBox TextBox2;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.Label Label2;
+    }
+}
+
